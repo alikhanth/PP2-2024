cff347424829f33e1d48f43dead0b021312314cc
Alikhan <alihan.tursynhan04@gmail.com>
2024-01-28 22:49:54 +0600

    W2

---
+++ lab2/PythonLists/exercises.py
@@ -0,0 +1,30 @@
+if 5 == 10 or 4 == 4:
+    print("At least one of the statements is true") 
+
+
+fruits = ["apple", "banana", "cherry"]
+fruits[0] = "kiwi" 
+
+
+fruits = ["apple", "banana", "cherry"]
+fruits.append("orange") 
+
+
+fruits = ["apple", "banana", "cherry"]
+fruits.insert(1,"lemon") 
+
+
+fruits = ["apple", "banana", "cherry"]
+fruits.remove("banana")
+
+
+fruits = ["apple", "banana", "cherry"]
+print(fruits[-1])
+
+
+fruits = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(fruits[2:5]) 
+
+
+fruits = ["apple", "banana", "cherry"]
+print(len(fruits))
